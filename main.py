a7f4527a488aa4a2f51a9f3a005ea3029e5468b7
MYT <99574999+IrisMYT@users.noreply.github.com>
2025-08-11 23:17:15 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from downloader.manager import DownloadManager
 if __name__ == "__main__":
     try:
         DownloadManager().run()
-    except KeyboardInterrupt:
+        except KeyboardInterrupt:
         print("
 Stopped by user.")
